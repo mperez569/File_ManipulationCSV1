feb0503d9867a4ea39119b7f6619f401a6a17511
mp230 <mp230@BOOK-K5PTLV2C4V>
2023-11-07 10:29:27 -0600

    Add project files.

---
+++ File_ManipulationCSV1/File_ManipulationCSV1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	// creates a csv file and takes user input to create an employee file
+	string id;
+	string full_name;
+	string age;
+
+	ofstream CSV_File("employees.csv");
+	cout << "Employee Data Entry" << endl;
+	cout << "===================" << endl;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Enter the information for Employee #" << i << endl;
+		cout << "Enter their ID: ";
+		cin >> id;
+		cin.ignore();
+		cout << "Enter their full name: ";
+		getline(cin, full_name);
+		cout << "Enter their age: ";
+		cin >> age;
+		CSV_File << id << ", " << full_name << ", " << age << endl;
+
+	}
+	CSV_File.close();
+}
