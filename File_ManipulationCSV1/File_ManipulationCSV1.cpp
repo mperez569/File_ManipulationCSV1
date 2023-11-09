afc1f9a48534657f9731aba44fecb2f393dd5aa6
mperez569 <mperez569@student.ccc.edu>
2023-11-09 09:48:16 -0600

    commit

--- File_ManipulationCSV1/File_ManipulationCSV1.cpp
+++ File_ManipulationCSV1/File_ManipulationCSV1.cpp
@@ -7,18 +7,20 @@ using namespace std;
 
 int main()
 {
-	// creates a csv file and takes user input to create an employee file
+	// creates a csv file and takes user input to create an employee csv file
 	string id;
 	string full_name;
 	string age;
 
+	//the ios::app checks if the file already exists, if it does, then it adds onto it, 
+	// but if it doesnt, then it creates a new file
 	ofstream CSV_File("employees.csv");
 	cout << "Employee Data Entry" << endl;
 	cout << "===================" << endl;
 
 	for (int i = 0; i < 5; i++)
 	{
-		cout << "Enter the information for Employee #" << i << endl;
+		cout << "Enter the information for Employee #" << i + 1 << endl;
 		cout << "Enter their ID: ";
 		cin >> id;
 		cin.ignore();
